92d5415603344505eec45d07b9ebf8c379d30733
Kyoko Kobe <49667068+kobekyoko@users.noreply.github.com>
2019-05-02 18:14:01 +0900

    Add files via upload

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json());
+app.get('/', (req, res) => {
+	res.send("getting root");
+});
+
+app.get('/profile', (req, res) => {
+	res.send("getting profile");
+});
+
+app.post('/profile', (req, res) => {
+	console.log(req.body)
+	const user = {
+		name: 'sally',
+		hobby: 'soccer'
+	}
+	res.send('Success');
+});
+
+app.listen(3000);
