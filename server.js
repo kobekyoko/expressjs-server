4495b405fa8e2e2d74b6145e5b435e0467410e32
Kyoko Kobe <49667068+kobekyoko@users.noreply.github.com>
2019-05-04 16:28:54 +0900

    Update server.js

--- server.js
+++ server.js
@@ -4,4 +4,28 @@ const bodyParser = require('body-parser');
 const app = express();
 app.use(express.static(__dirname + '/public'))
 
+//app.use(bodyParser.urlencoded({extended: false}))
+//app.use(bodyParser.json());
+
+//app.get('/:id', (req, res) => {
+	//console.log(req.query)
+	//req.body
+	//console.log(req.header)
+	//console.log(req.params)
+	//res.status(404).send("not found");
+//});
+
+//app.get('/profile', (req, res) => {
+	//res.send("getting profile");
+//});
+
+//app.post('/profile', (req, res) => {
+	//console.log(req.body)
+	//const user = {
+		//name: 'sally',
+		//hobby: 'soccer'
+	//}
+	//res.send('Success');
+//});
+
 app.listen(3000);
